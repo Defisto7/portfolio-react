f72656b237630485fb5247aa52fbe68eaf047389
Dima Yanovskji <criska7@gmail.com>
2023-01-08 20:27:25 +0200

    Master
    add router and active link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./css/style.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
@@ -10,12 +12,16 @@ import ProjectPage from "./pages/ProjectPage";
 const App = () => {
   return (
     <div className="App">
+      <Router>
       <Navbar />
-      {/* <Home /> */}
-      <ProjectPage />
-      {/* <Skills /> */}
-      {/* <Contacts/> */}
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/projects" element={<ProjectPage />}/>
+        <Route path="/contacts" element={<Contacts/>}/>
+        <Route path="/skills" element={<Skills />}/>
+      </Routes>
       <Footer />
+      </Router>
     </div>
   );
 };
