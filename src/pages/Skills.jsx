a7bda30619d360e1bc35178068b10d5390c8fa27
Dima Yanovskji <criska7@gmail.com>
2023-01-10 02:04:46 +0200

    Master
    add tools

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -8,6 +8,10 @@ const Skills = () => {
           <h2 className="title-2">Frontend</h2>
           <p>JavaScript, TypeScript, ReactJS, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, TailwindCSS</p>
         </li>
+        <li className="content-list__item">
+          <h2 className="title-2">Tools</h2>
+          <p>Git & GitHub, NPM, Chrome Development Tools, Photoshop, VSCode, Figma, WebStorm</p>
+        </li>
       </ul>
     </div>
   </main>
