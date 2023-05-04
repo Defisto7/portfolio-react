e333876f58a610c484b8c9f5f151c84c570eb732
Dima Yanovskji <criska7@gmail.com>
2023-05-04 10:24:15 +0300

    Master
    add project and change img

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,85 +1,120 @@
 import project01 from "./../img/projects/01.jpg";
-import project01Big from "./../img/projects/01-big.jpg";
-import project02 from "./../img/projects/02.jpg";
 import project02Big from "./../img/projects/02-big.jpg";
-import project03 from "./../img/projects/03.jpg";
 import project03Big from "./../img/projects/03-big.jpg";
-import project04 from "./../img/projects/04.jpg";
 import project04Big from "./../img/projects/04-big.jpg";
-import project05 from "./../img/projects/05.jpg";
-import project05Big from "./../img/projects/05-big.jpg";
+import project06Big from "./../img/projects/05-big.jpg";
 import project06 from "./../img/projects/06.jpg";
+import project05Big from "./../img/projects/06-big.jpg";
 import project07 from "./../img/projects/07.jpg";
 import project07Big from "./../img/projects/07-big.jpg";
 import project08 from "./../img/projects/08.jpg";
 import project08Big from "./../img/projects/08-big.jpg";
-
+import project09 from "./../img/projects/09.jpg";
+import project09Big from "./../img/projects/09-big.jpg";
+import project10 from "./../img/projects/10.jpg";
+import project10Big from "./../img/projects/10-big.jpg";
+import project11 from "./../img/projects/11.jpg";
+import project11Big from "./../img/projects/11-big.jpg";
+import project12Big from "./../img/projects/12-big.jpg";
 
 const project = [
     {
-        title: 'Evogym',
-        skills: 'React, Tailwind css, typescript',
+        title: "Evogym",
+        skills: "React, Tailwind css, typescript",
+        img: project12Big,
+        imgBig: project12Big,
+        gitHubLink: "https://github.com/Defisto7/gym-typescript",
+        demoLink: "https://gym-typescript-ebon.vercel.app/",
+    },
+    {
+        title: "Internet-store-energy",
+        skills: "React, Redux",
+        img: project11,
+        imgBig: project11Big,
+        gitHubLink: "https://github.com/Defisto7/internet-store-energy",
+        demoLink: "https://internet-store-energy.vercel.app/",
+    },
+    {
+        title: "Modern bank",
+        skills: "React, Tailwind css",
+        img: project10,
+        imgBig: project10Big,
+        gitHubLink: "https://github.com/Defisto7/bank_modern_app",
+        demoLink: "https://bank-modern-app-tau.vercel.app/",
+    },
+    {
+        title: "Admin dashboard",
+        skills: "React, Material UI, Nivo Charts, Formik, Yup, FullCalendar, and Data Grid",
+        img: project09,
+        imgBig: project09Big,
+        gitHubLink: "https://github.com/Defisto7/react-admin-dashboard",
+        demoLink: "https://react-admin-dashboard-ivory.vercel.app/",
+    },
+    {
+        title: "Netflix",
+        skills: "React, Tailwind css",
         img: project08,
         imgBig: project08Big,
-        gitHubLink: 'https://github.com/Defisto7/gym-typescript',
-        demoLink: 'https://gym-typescript-ebon.vercel.app/'
+        gitHubLink: "https://github.com/Defisto7/netflix_react",
+        demoLink: "https://netflix-react-ashy-gamma.vercel.app/",
     },
     {
-        title: 'Modern bank',
-        skills: 'React, Tailwind css',
-        img: project01,
-        imgBig: project01Big,
-        gitHubLink: 'https://github.com/Defisto7/bank_modern_app',
-        demoLink: 'https://bank-modern-app-tau.vercel.app/'
-    },
-    {
-        title: 'Admin dashboard',
-        skills: 'React, Material UI, Nivo Charts, Formik, Yup, FullCalendar, and Data Grid',
-        img: project02,
-        imgBig: project02Big,
-        gitHubLink: 'https://github.com/Defisto7/react-admin-dashboard',
-        demoLink: 'https://react-admin-dashboard-ivory.vercel.app/'
-    },
-    {
-        title: 'Netflix',
-        skills: 'React, Tailwind css',
-        img: project03,
-        imgBig: project03Big,
-        gitHubLink: 'https://github.com/Defisto7/netflix_react',
-        demoLink: 'https://netflix-react-ashy-gamma.vercel.app/'
-    },
-    {
-        title: 'Youtube',
-        skills: 'React, MUI',
-        img: project04,
-        imgBig: project04Big,
-        gitHubLink: 'https://github.com/Defisto7/youtube_clone',
-        demoLink: 'https://youtube-clone-xi-three.vercel.app/'
-    },
-    {
-        title: 'Game store',
-        skills: 'React, Redux',
-        img: project05,
-        imgBig: project05Big,
-        gitHubLink: 'https://github.com/Defisto7/games-store',
-        demoLink: 'https://games-store-orpin.vercel.app/'
-    },
-    {
-        title: 'Bejeweled',
-        skills: 'HTML, SCSS, BEM',
-        img: project06,
-        imgBig: project06,
-        gitHubLink: 'https://github.com/Defisto7/Bejeweled',
-        demoLink: 'https://defisto7.github.io/Bejeweled/'
-    },
-    {
-        title: 'Internet-store-energy',
-        skills: 'React, Redux',
+        title: "Youtube",
+        skills: "React, MUI",
         img: project07,
         imgBig: project07Big,
-        gitHubLink: 'https://github.com/Defisto7/internet-store-energy',
-        demoLink: 'https://internet-store-energy.vercel.app/'
+        gitHubLink: "https://github.com/Defisto7/youtube_clone",
+        demoLink: "https://youtube-clone-xi-three.vercel.app/",
     },
-]
+    {
+        title: "Game store",
+        skills: "React, Redux",
+        img: project06,
+        imgBig: project06Big,
+        gitHubLink: "https://github.com/Defisto7/games-store",
+        demoLink: "https://games-store-orpin.vercel.app/",
+    },
 
-export {project}
+    {
+        title: "Loan",
+        skills: "Gulp, JavaScript",
+        img: project05Big,
+        imgBig: project05Big,
+        gitHubLink: "https://github.com/Defisto7/Loan-dist",
+        demoLink: "https://defisto7.github.io/Loan-dist/",
+    },
+    {
+        title: "desire",
+        skills: "HTML, SCSS, BEM, Gulp",
+        img: project04Big,
+        imgBig: project04Big,
+        gitHubLink: "https://github.com/Defisto7/desire",
+        demoLink: "https://defisto7.github.io/desire-dist/",
+    },
+    {
+        title: "Mercedes-Benz concept",
+        skills: "HTML, SCSS, BEM",
+        img: project03Big,
+        imgBig: project03Big,
+        gitHubLink: "https://github.com/Defisto7/Avtr",
+        demoLink: "https://defisto7.github.io/Avtr/",
+    },
+    {
+        title: "AUTO FROM USA",
+        skills: "HTML, SCSS, BEM, Gulp",
+        img: project02Big,
+        imgBig: project02Big,
+        gitHubLink: "https://github.com/Defisto7/Cars",
+        demoLink: "https://defisto7.github.io/Cars/",
+    },
+    {
+        title: "Bejeweled",
+        skills: "HTML, SCSS, BEM",
+        img: project01,
+        imgBig: project01,
+        gitHubLink: "https://github.com/Defisto7/Bejeweled",
+        demoLink: "https://defisto7.github.io/Bejeweled/",
+    },
+];
+
+export { project };
