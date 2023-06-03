a3942dab990df843324cb2a930957980337cde65
Dima Yanovskji <criska7@gmail.com>
2023-06-03 23:23:03 +0300

    Master
    fix img

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -3,7 +3,7 @@ import project02Big from "./../img/projects/02-big.jpg";
 import project03Big from "./../img/projects/03-big.jpg";
 import project04Big from "./../img/projects/04-big.jpg";
 import project05Big from "./../img/projects/05-big.jpg";
-import project06Big from "./../img/projects/05-big.jpg";
+import project06Big from "./../img/projects/06-big.jpg";
 import project06 from "./../img/projects/06.jpg";
 import project07 from "./../img/projects/07.jpg";
 import project07Big from "./../img/projects/07-big.jpg";
